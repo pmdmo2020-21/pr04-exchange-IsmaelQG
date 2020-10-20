5d203556c54118cb382f4a85a7d78121b4010c40
Ismael Quirós <ismaeljesusquiros@gmail.com>
2020-10-20 13:10:44 +0200

    v0.2

--- app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
@@ -16,10 +17,20 @@ import es.iessaladillo.pedrojoya.exchange.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding: MainActivityBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = MainActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        setupViews()
     }
+
+    private fun setupViews(){
+        //binding!!.btnExchange.setOnClickListener {v: View? -> btnOnClick()}
+    }
+
+
+
 
 }
