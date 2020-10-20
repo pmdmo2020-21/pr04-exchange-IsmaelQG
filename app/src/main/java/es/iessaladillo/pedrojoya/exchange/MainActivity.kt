c564d31b321a3da4695457c9db8db76f1e8d452e
Ismael <ismaeljesusquiros@gmail.com>
2020-10-20 20:43:00 +0200

    v0.4
    Avances generales

--- app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
@@ -1,6 +1,8 @@
 package es.iessaladillo.pedrojoya.exchange
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.ImageView
@@ -11,26 +13,108 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.text.set
 import androidx.core.view.ViewCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doAfterTextChanged
 import es.iessaladillo.pedrojoya.exchange.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding: MainActivityBinding? = null
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-        setupViews()
+        setContentView(binding.root)
+        fromCurrencyCheck()
+        toCurrencyCheck()
+        start()
     }
 
-    private fun setupViews(){
-        //binding!!.btnExchange.setOnClickListener {v: View? -> btnOnClick()}
+    private fun start(){
+        binding.txtAmount.selectAll()
+        binding.rdbFromCurrencyEuro.isChecked = true
+        binding.rdbToCurrencyDollar.isChecked = true
+        changeFromCurrencyCheckImg(R.id.rdbFromCurrencyEuro)
+        changeFromCurrencyCheckImg(R.id.rdbToCurrencyDollar)
+        money()
     }
 
+    fun toEditable(i: String): Editable {
+        return Editable.Factory.getInstance().newEditable(i)
+    }
 
+    private fun money(){
+        binding.txtAmount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (binding.txtAmount.text.isBlank()){
+                    binding.txtAmount.text = toEditable("0")
+                    binding.txtAmount.selectAll()
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+    }
+
+    private fun fromCurrencyCheck(){
+        binding.rdgFromCurrency.setOnCheckedChangeListener(){ view, idButton -> changeFromCurrencyCheckImg(idButton)}
+    }
+
+    private fun changeFromCurrencyCheckImg(idButton : Int){
+        when(idButton){
+            R.id.rdbFromCurrencyDollar -> {
+                binding.imgFromCurrency.setImageResource(R.drawable.ic_dollar)
+                binding.rdbToCurrencyDollar.isEnabled = false
+                binding.rdbToCurrencyEuro.isEnabled = true
+                binding.rdbToCurrencyPound.isEnabled = true
+            }
+            R.id.rdbFromCurrencyEuro -> {
+                binding.imgFromCurrency.setImageResource(R.drawable.ic_euro)
+                binding.rdbToCurrencyDollar.isEnabled = true
+                binding.rdbToCurrencyEuro.isEnabled = false
+                binding.rdbToCurrencyPound.isEnabled = true
+            }
+            R.id.rdbFromCurrencyPound -> {
+                binding.imgFromCurrency.setImageResource(R.drawable.ic_pound)
+                binding.rdbToCurrencyDollar.isEnabled = true
+                binding.rdbToCurrencyEuro.isEnabled = true
+                binding.rdbToCurrencyPound.isEnabled = false
+            }
+        }
+    }
+
+    private fun toCurrencyCheck(){
+        binding.rdgToCurrency.setOnCheckedChangeListener(){ view, idButton -> changeToCurrencyCheckImg(idButton)}
+    }
+
+    private fun changeToCurrencyCheckImg(idButton : Int){
+        when(idButton){
+            R.id.rdbToCurrencyDollar -> {
+                binding.imgToCurrency.setImageResource(R.drawable.ic_dollar)
+                binding.rdbFromCurrencyDollar.isEnabled = false
+                binding.rdbFromCurrencyEuro.isEnabled = true
+                binding.rdbFromCurrencyPound.isEnabled = true
+            }
+            R.id.rdbToCurrencyEuro -> {
+                binding.imgToCurrency.setImageResource(R.drawable.ic_euro)
+                binding.rdbFromCurrencyDollar.isEnabled = true
+                binding.rdbFromCurrencyEuro.isEnabled = false
+                binding.rdbFromCurrencyPound.isEnabled = true
+            }
+            R.id.rdbToCurrencyPound -> {
+                binding.imgToCurrency.setImageResource(R.drawable.ic_pound)
+                binding.rdbFromCurrencyDollar.isEnabled = true
+                binding.rdbFromCurrencyEuro.isEnabled = true
+                binding.rdbFromCurrencyPound.isEnabled = false
+            }
+        }
+    }
 
 
 }
